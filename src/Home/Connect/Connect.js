18c709bf3dec75d642ea8c1ca7772bbfb697adc2
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-04-07 16:50:05 +0600

    something change

--- src/Home/Connect/Connect.js
+++ src/Home/Connect/Connect.js
@@ -7,6 +7,7 @@ const Connect = () => {
             <h1 className='text-4xl font-semibold'>Let's Connect</h1>
             <div className='flex justify-center'>
                 <button className='tracking-widest font-semibold border border-indigo-400  px-5 py-2 md:px-10 md:py-3 ml-3 text-indigo-400 transition duration-300 hover:shadow-indigo-900 hover:bg-indigo-500 hover:rounded-md hover:text-white hover:shadow-2xl my-5'>GET IN TOCH</button>
+
             </div>
         </div>
     );
